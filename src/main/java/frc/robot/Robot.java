c5c42b6bd296049a15d2594d4592c11eca22b215
alya222 <alya_nimis-ibrahim@isd31.net>
2022-01-07 16:35:32 -0600

    fixing commit issue

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,14 +1,16 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -18,7 +20,6 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
   private RobotContainer m_robotContainer;
 
   /**
@@ -30,14 +31,18 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    m_robotContainer.init();
+
+    //CameraServer.getInstance().startAutomaticCapture("Inside View", 0);
+    //CameraServer.getInstance().startAutomaticCapture("Shooter View", 1);
   }
 
   /**
    * This function is called every robot packet, no matter the mode. Use this for items like
    * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
    *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
    */
   @Override
   public void robotPeriodic() {
@@ -46,17 +51,22 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    m_robotContainer.periodic();
+  }
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
+  public void disabledInit() {
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledPeriodic() {
+  }
 
-  @Override
-  public void disabledPeriodic() {}
-
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
- /*
+  /**
+   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
@@ -66,10 +76,14 @@ public class Robot extends TimedRobot {
       m_autonomousCommand.schedule();
     }
   }
-*/
-  /** This function is called periodically during autonomous. */
+  //Whatup
+
+  /**
+   * This function is called periodically during autonomous.
+   */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
   public void teleopInit() {
@@ -82,9 +96,12 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called periodically during operator control. */
+  /**
+   * This function is called periodically during operator control.
+   */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
@@ -92,7 +109,10 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
+  /**
+   * This function is called periodically during test mode.
+   */
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 }
--- src/main/java/frc/robot/drive/RevDrivetrain.java
+++ src/main/java/frc/robot/drive/RevDrivetrain.java
@@ -7,15 +7,30 @@
 
 package frc.robot.drive;
 
+import com.kauailabs.navx.frc.AHRS;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SlewRateLimiter;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpiutil.math.MathUtil;
+
+import static frc.robot.Gains.*;
 
 import static frc.robot.Constants.*;
 
-
+/**
+ * Add your docs here.
+ */
 public class RevDrivetrain extends SubsystemBase {
 
   private CANSparkMax LFrontWheel = new CANSparkMax(kLeftFrontPort, MotorType.kBrushless);
@@ -26,17 +41,135 @@ public class RevDrivetrain extends SubsystemBase {
 
   private DifferentialDrive roboDrive = new DifferentialDrive(LFrontWheel, RFrontWheel);
 
+
+  //private AHRS gyro = new AHRS(SPI.Port.kMXP);
+
+  //private double gyroPosition = -gyro.getAngle();
+
+  //private PIDController gyroController 
+    //= new PIDController(0.1, 0, 0);
+
+  // Autonomous Tracking
+  //private DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(kTrackWidthMeters);
+  //private DifferentialDriveOdometry odometry = new DifferentialDriveOdometry(getHeading());
+
+  private SimpleMotorFeedforward feedforward 
+    = new SimpleMotorFeedforward(driveFeedforward.ks, driveFeedforward.kv, driveFeedforward.ka);
+
+  private PIDController leftDrivePID 
+    = new PIDController(leftDrive.kP, leftDrive.kI, leftDrive.kD);
+
+  private PIDController rightDrivePID
+    = new PIDController(rightDrive.kP, rightDrive.kI, rightDrive.kD);
+
+  private Pose2d pose = new Pose2d();
+
+  private SlewRateLimiter speedLimiter = new SlewRateLimiter(3);
+
+  private SlewRateLimiter leftLimiter = new SlewRateLimiter(0.3);
+  private SlewRateLimiter rightLimiter = new SlewRateLimiter(0.3);
+
   public RevDrivetrain() {
+    //This Will remove the Motors not updated enought warning:
+    //roboDrive.setSafetyEnabled(false);
+
     
     LRearWheel.follow(LFrontWheel);
     RRearWheel.follow(RFrontWheel);
+
+    LFrontWheel.getEncoder().setPosition(0);
+    RFrontWheel.getEncoder().setPosition(0);
+
+    //gyro.reset();
+  }
+
+  public void limiterDrive(double leftPercent, double rightPercent) {
+    roboDrive.tankDrive(leftLimiter.calculate(leftPercent), rightLimiter.calculate(rightPercent), false);
+  }
+
+  public double deadband(double JoystickValue, double DeadbandCutOff) {
+    double deadbandreturn;
+
+    if (Math.abs(JoystickValue) < DeadbandCutOff) {
+      deadbandreturn = 0;
+    }
+    else {
+      deadbandreturn = JoystickValue;
+    }
+    
+    return deadbandreturn;
+  }
+  
+  public void setOutputVolts(double leftVolts, double rightVolts) {
+    LFrontWheel.setVoltage(leftVolts);
+    RFrontWheel.setVoltage(rightVolts);
+  }  
+
+  public void setOutputPercent(double leftPercent, double rightPercent) {
+    LFrontWheel.set(leftPercent);
+    RFrontWheel.set(rightPercent);
+  }  
+
+  public void setOutputFeedforward(double leftVolts, double rightVolts) {
+    LFrontWheel.setVoltage(feedforward.calculate(leftVolts));
+    RFrontWheel.setVoltage(feedforward.calculate(rightVolts));
   }
 
   public DifferentialDrive getDifferentialDrive() {
     return roboDrive;
   }
+
+  /*public double getAngle() {
+    return -gyro.getAngle();
+  }
+
+  public Rotation2d getHeading() {
+    return Rotation2d.fromDegrees(getAngle());
+  }
+
+  public DifferentialDriveKinematics getKinematics() {
+    return kinematics;
+  }
   
+  public Pose2d getPose() {
+    return pose;
+  }
+
+  public SimpleMotorFeedforward getFeedforward() {
+    return feedforward;
+  }
+
+  public PIDController getLeftDrivePID() {
+    return leftDrivePID;
+  }
+
+  public PIDController getRightDrivePID() {
+    return rightDrivePID;
+  }
+  
+  public double getLeftDistanceMeters() {
+    return LFrontWheel.getEncoder().getPosition() / 
+    RFrontWheel.getEncoder().getCountsPerRevolution() * 2 * Math.PI * kDriveWheelRadiusMeters;
+  }
+
+  public double getRightDistanceMeters() {
+    return RFrontWheel.getEncoder().getPosition() / 
+    RFrontWheel.getEncoder().getCountsPerRevolution() * 2 * Math.PI * kDriveWheelRadiusMeters;
+      
+  }
+
+  public DifferentialDriveWheelSpeeds getSpeeds() {
+    return new DifferentialDriveWheelSpeeds(
+      LFrontWheel.getEncoder().getVelocity() / kGearRatio * 2 * Math.PI * kDriveWheelRadiusMeters / 60,
+      RFrontWheel.getEncoder().getVelocity() / kGearRatio * 2 * Math.PI * kDriveWheelRadiusMeters / 60
+    );
+  } */
+
+  /**
+  * Will be called periodically whenever the CommandScheduler runs.
+  */
   @Override
   public void periodic() {
+      //pose = odometry.update(getHeading(), getLeftDistanceMeters(), getRightDistanceMeters());
   }
 }
